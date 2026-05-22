f6ca4b0af59def01c208531c8d84a79c3ede18d0
mystricman0-cell <mystricman0@gmail.com>
2026-05-22 09:12:13 +0530

    Fix: RONALDO_MUSIC/plugins/tools/ping.py

--- RONALDO_MUSIC/plugins/tools/ping.py
+++ RONALDO_MUSIC/plugins/tools/ping.py
@@ -1,36 +1,55 @@
 from datetime import datetime
+
+import aiohttp
 from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+import config
 from RONALDO_MUSIC import app
 from RONALDO_MUSIC.core.call import NOBITA
 from RONALDO_MUSIC.utils import bot_sys_stats
-from RONALDO_MUSIC.utils.inline.extras import botplaylist_markup
 from RONALDO_MUSIC.utils.decorators.language import language
+from RONALDO_MUSIC.utils.inline.extras import botplaylist_markup
 from config import BANNED_USERS, PING_IMG_URL
-import aiohttp
-import asyncio
 
- 
-@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS & filters.group)
+
+@app.on_message(filters.command(["ping", "alive"]) & ~BANNED_USERS)
 @language
 async def ping_com(client, message: Message, _):
-    response = await message.reply_photo(
-        photo=PING_IMG_URL,
-        caption=_["ping_1"].format(app.mention),
-    )
+    try:
+        response = await message.reply_photo(
+            photo=PING_IMG_URL,
+            caption=_["ping_1"].format(app.mention),
+        )
+    except Exception:
+        response = await message.reply_text(_["ping_1"].format(app.mention))
+
     start = datetime.now()
-    pytgping = await NOBITA.ping()
-    UP, CPU, RAM, DISK = await bot_sys_stats()
+    try:
+        pytgping = await NOBITA.ping()
+    except Exception:
+        pytgping = 0
+
+    try:
+        UP, CPU, RAM, DISK = await bot_sys_stats()
+    except Exception:
+        UP, CPU, RAM, DISK = "N/A", "N/A", "N/A", "N/A"
+
     resp = (datetime.now() - start).microseconds / 1000
-    await response.edit(
-        _["ping_2"].format(
-            resp,
-            app.mention,
-            UP,
-            RAM,
-            CPU,
-            DISK,
-            pytgping,
-        ),
-        reply_markup=InlineKeyboardMarkup(botplaylist_markup(_)),
+    try:
+        await response.edit(
+            _["ping_2"].format(
+                resp,
+                app.mention,
+                UP,
+                RAM,
+                CPU,
+                DISK,
+                pytgping,
+            ),
+            reply_markup=InlineKeyboardMarkup(botplaylist_markup(_)),
+        )
+    except Exception:
+        await response.edit_text(
+            f"🏓 Pong!\n\nLatency: {resp}ms\nUptime: {UP}"
         )
