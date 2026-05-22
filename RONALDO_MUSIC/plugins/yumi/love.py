181451db30f4ef4eb1107e033feaf0ce88a70567
mystricman0-cell <mystricman0@gmail.com>
2026-05-22 09:16:10 +0530

    Fix async: RONALDO_MUSIC/plugins/yumi/love.py

--- RONALDO_MUSIC/plugins/yumi/love.py
+++ RONALDO_MUSIC/plugins/yumi/love.py
@@ -23,16 +23,14 @@ def get_random_message(love_percentage):
         ])
         
 @app.on_message(filters.command("love", prefixes="/"))
-def love_command(client, message):
+async def love_command(client, message):
     command, *args = message.text.split(" ")
     if len(args) >= 2:
         name1 = args[0].strip()
         name2 = args[1].strip()
-        
         love_percentage = random.randint(10, 100)
         love_message = get_random_message(love_percentage)
-
         response = f"{name1}💕 + {name2}💕 = {love_percentage}%\n\n{love_message}"
     else:
         response = "Please enter two names after /love command."
-    app.send_message(message.chat.id, response)
+    await message.reply(response)
