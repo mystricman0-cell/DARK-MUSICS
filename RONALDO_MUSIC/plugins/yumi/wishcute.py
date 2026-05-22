a0df02a1767451c419bbeecd498752868ddff7fb
mystricman0-cell <mystricman0@gmail.com>
2026-05-22 09:21:33 +0530

    Rebrand: @TelephoneVirus in RONALDO_MUSIC/plugins/yumi/wishcute.py

--- RONALDO_MUSIC/plugins/yumi/wishcute.py
+++ RONALDO_MUSIC/plugins/yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from RONALDO_MUSIC import app 
 
-SUPPORT_CHAT = "NOB1TA_SUPPORT"
+SUPPORT_CHAT = "TelephoneVirus"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
