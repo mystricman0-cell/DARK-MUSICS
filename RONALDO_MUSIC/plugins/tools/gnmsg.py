cec9fe654dee68f93fb7a04ef0167dbf97eb9a1f
mystricman0-cell <mystricman0@gmail.com>
2026-05-22 09:16:51 +0530

    Fix async: RONALDO_MUSIC/plugins/tools/gnmsg.py

--- RONALDO_MUSIC/plugins/tools/gnmsg.py
+++ RONALDO_MUSIC/plugins/tools/gnmsg.py
@@ -1,28 +1,13 @@
-import re
-from pyrogram import filters
 import random
+from pyrogram import filters
 from RONALDO_MUSIC import app
-
-
-@app.on_message(filters.command(["gn","n","oodnight","ood Night","ood night"], prefixes=["/","g","G"]))
-def goodnight_command_handler(_, message):
-    sender = message.from_user.mention
-    send_sticker = random.choice([True, False])
-    if send_sticker:
-        sticker_id = get_random_sticker()
-        app.send_sticker(message.chat.id, sticker_id)
-        message.reply_text(f"**Goodnight, {sender}! Sleep tight. 🌙**")
-    else:
-        emoji = get_random_emoji()
-        app.send_message(message.chat.id, emoji)
-        message.reply_text(f"**Goodnight, {sender}! Sleep tight. {emoji}**")
 
 
 def get_random_sticker():
     stickers = [
-        "CAACAgQAAx0Ce9_hCAACaEVlwn7HeZhgwyVfKHc3WUGC_447IAACLgwAAkQwKVPtub8VAR018x4E", # Sticker 1
-        "CAACAgIAAx0Ce9_hCAACaEplwn7dvj7G0-a1v3wlbN281RMX2QACUgwAAligOUoi7DhLVTsNsh4E", # Sticker 2
-        "CAACAgIAAx0Ce9_hCAACaFBlwn8AAZNB9mOUvz5oAyM7CT-5pjAAAtEKAALa7NhLvbTGyDLbe1IeBA", # Sticker 3
+        "CAACAgQAAx0Ce9_hCAACaEVlwn7HeZhgwyVfKHc3WUGC_447IAACLgwAAkQwKVPtub8VAR018x4E",
+        "CAACAgIAAx0Ce9_hCAACaEplwn7dvj7G0-a1v3wlbN281RMX2QACUgwAAligOUoi7DhLVTsNsh4E",
+        "CAACAgIAAx0Ce9_hCAACaFBlwn8AAZNB9mOUvz5oAyM7CT-5pjAAAtEKAALa7NhLvbTGyDLbe1IeBA",
         "CAACAgUAAx0CcmOuMwACldVlwn9ZHHF2-S-CuMSYabwwtVGC3AACOAkAAoqR2VYDjyK6OOr_Px4E",
         "CAACAgIAAx0Ce9_hCAACaFVlwn-fG58GKoEmmZpVovxEj4PodAACfwwAAqozQUrt2xSTf5Ac4h4E",
     ]
@@ -30,9 +15,20 @@ def get_random_sticker():
 
 
 def get_random_emoji():
-    emojis = [
-        "😴",
-        "😪",
-        "💤",
-    ]
-    return random.choice(emojis)
+    return random.choice(["😴", "😪", "💤"])
+
+
+@app.on_message(filters.command(["gn", "goodnight"], prefixes=["/", "g", "G"]))
+async def goodnight_command_handler(_, message):
+    sender = message.from_user.mention
+    send_sticker = random.choice([True, False])
+    try:
+        if send_sticker:
+            sticker_id = get_random_sticker()
+            await app.send_sticker(message.chat.id, sticker_id)
+            await message.reply_text(f"**Goodnight, {sender}! Sleep tight. 🌙**")
+        else:
+            emoji = get_random_emoji()
+            await message.reply_text(f"**Goodnight, {sender}! Sleep tight. {emoji}**")
+    except Exception as e:
+        await message.reply_text(f"Goodnight, {sender}! 🌙")
